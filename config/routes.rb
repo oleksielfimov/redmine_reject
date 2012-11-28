6778c9d1729277d8df5e477585e5e1626066e680
root <root@oleksiivm.(none)>
2012-11-28 17:26:00 +0200

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
- ActionController::Routing::Routes.draw do |map|
-   map.connect '/issues/reject/:id', :controller => 'issues', :action => 'edit', :isRejected => true
- end
+if Rails::VERSION::MAJOR >= 3
+  RedmineApp::Application.routes.draw do
+    match '/issues/:id/assign_reject', :controller => 'issues', :action => 'edit', :isRejected => true
+  end
+else
+  ActionController::Routing::Routes.draw do |map|
+    map.connect '/issues/:id/edit', :controller => 'issues', :action => 'edit', :isRejected => true
+  end
+end
 
