78bb4d4417f5ccd4d6c0ab5456115ee01bad1db1
Oleksii <Oleksii.ielfimov@gmail.com>
2012-12-01 19:05:52 +0200

    controller change

--- lib/issues_controller_patch.rb
+++ lib/issues_controller_patch.rb
@@ -5,14 +5,31 @@ module IssuesControllerPatch
     base.send(:include, InstanceMethods)
     base.class_eval do
       before_filter :if_reject, :only => [:edit]
+      before_filter :reject, :only => [:edit]
+      before_filter :reject_assign, :only => [:edit]
+      
     end
   end
   
   module InstanceMethods
     def if_reject
       if params[:reject]
-        @reject_id = @issue.id
+        @reject_id = true
+        @issue_for_reject = Issue.find(params[:id])
       end
+    end
+    def reject
+      Journal.find(:all, :conditions => "journalized_id = #{@issue_for_reject.id}").collect {|t| t.id}.each do |journal|
+        @reject_user_id = JournalDetail.find(:all, :conditions => ("value = #{User.current.id}" && id = journal), :order => "id DESC").collect {|u| u.old_value if u.prop_key == 'assigned_to_id' }.first 
+        if @reject_user_id.nil?
+           @user_reject_id = @issue_for_reject.author_id
+        else
+          @user_reject_id = @reject_user_id 
+        end
+      end
+    end
+    def reject_assign
+      @user_reject = User.find(:all, :conditions => "id = #{@user_reject_id}").collect {|i| [i.name]}
     end
   end
 end
--- lib/issues_patch.rb
+++ lib/issues_patch.rb
@@ -5,16 +5,14 @@ module IssuesPatch
   
   module InstanceMethods
     # Returns previous assigned user
-    def reject
-      Journal.find(:all, :conditions => "journalized_id = #{@issue.id}").collect {|t| t.id}.each do |journal|
-        return JournalDetail.find(:all, :conditions => (value = '#{User.current.id}' && id = journal), :order => "id DESC").collect {|u| u.old_value if u.prop_key == 'assigned_to_id' }.first 
-      end
-    end
-    def reject_assign
-      User.find(:all, :conditions => "id = #{reject}").collect {|i| [i.name]}
-    end
+    
   end
 end
+
+
+
+
+
 
 
 
